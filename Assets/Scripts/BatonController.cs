9470d5b11674fd4a925382881dacd6e291720659
kencul <113139701+kencul@users.noreply.github.com>
2023-05-11 02:10:24 -0400

    final push

--- Assets/Scripts/BatonController.cs
+++ Assets/Scripts/BatonController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using OscJack;
 
 public class BatonController : MonoBehaviour
 {
@@ -10,16 +11,44 @@ public class BatonController : MonoBehaviour
     [SerializeField] GameObject BottomPoint;
     [SerializeField] Slider TopXSlider, TopYSlider, TopZSlider;
 
+    [SerializeField] GameObject[] Keys;
+
+    //OSC
+    readonly OscClient client = new("127.0.0.1", 57121);
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        client.Send("/panic");
     }
 
     // Update is called once per frame
     void Update()
     {
         //Assign the position of the top empty object of the baton to the sliders
-        (TopXSlider.value, TopYSlider.value, TopZSlider.value) = (TopPoint.transform.position.x, TopPoint.transform.position.y, TopPoint.transform.position.z);
+        //(TopXSlider.value, TopYSlider.value, TopZSlider.value) = (TopPoint.transform.position.x, TopPoint.transform.position.y, TopPoint.transform.position.z);
+
+        //If top point has moved, send OSC
+        if (TopPoint.transform.hasChanged)
+        {
+            client.Send("/pan", Remap(TopPoint.transform.position.x, -3f, 3f, -1f, 1f));
+            client.Send("/oscfreq", Remap(TopPoint.transform.position.z, -1f, 1f, 20f, 20000f));
+            client.Send("/vib", Remap(TopPoint.transform.position.y, 0f, 2f, 0f, 30f));
+        }
+    }
+
+    public void KeyboardTouch(int note)
+    {
+        client.Send("/on", note);
+    }
+
+    public void KeyboardRelease(int note)
+    {
+        client.Send("/off", note);
+    }
+
+    float Remap(float source, float sourceFrom, float sourceTo, float targetFrom, float targetTo)
+    {
+        return targetFrom + (source - sourceFrom) * (targetTo - targetFrom) / (sourceTo - sourceFrom);
     }
 }
--- Assets/Scripts/OSCMessager.cs
+++ Assets/Scripts/OSCMessager.cs
@@ -5,32 +5,75 @@ using OscJack;
 
 public class OSCMessager : MonoBehaviour
 {
+    readonly OscClient client = new("127.0.0.1", 57120);
+    private Dictionary<KeyCode, int> keyToMidi = new()
+    {
+        { KeyCode.A, 60},
+        { KeyCode.W, 61},
+        { KeyCode.S, 62},
+        { KeyCode.E, 63},
+        { KeyCode.D, 64},
+        { KeyCode.F, 65},
+        { KeyCode.T, 66},
+        { KeyCode.G, 67},
+        { KeyCode.Y, 68},
+        { KeyCode.H, 69},
+        { KeyCode.U, 70},
+        { KeyCode.J, 71},
+        { KeyCode.K, 72},
+    };
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //StartCoroutine(Test());
+        client.Send("/panic");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        foreach (KeyValuePair<KeyCode,int> entry in keyToMidi)
         {
-            // IP address, port number
-            using var client = new OscClient("127.0.0.1", 57120);
-            client.Send("/on",       // OSC address
-                        60); // Second element
+            if (Input.GetKeyDown(entry.Key))
+            {
+                // IP address, port number
+                client.Send("/on",       // OSC address
+                            entry.Value); // First element
+            }
+            if (Input.GetKeyUp(entry.Key))
+            {
+                // IP address, port number
+                client.Send("/off",       // OSC address
+                            entry.Value); // First element
+            }
         }
 
-        if (Input.GetKeyUp(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            // IP address, port number
-            using var client = new OscClient("127.0.0.1", 57120);
-            client.Send("/off",       // OSC address
-                        60); // First element
+            client.Send("/panic");
         }
     }
+
+    /*IEnumerator Test()
+    {
+        using var client = new OscClient("127.0.0.1", 57120);
+        
+
+        while (true)
+        {
+            float note = Random.Range(50, 80);
+            yield return new WaitForSeconds(0.5f);
+            
+            client.Send("/on",       // OSC address
+                        note); // First element
+
+            yield return new WaitForSeconds(0.5f);
+
+            client.Send("/off",       // OSC address
+                        note); // Second element
+        }
+    }*/
 
 
 }
